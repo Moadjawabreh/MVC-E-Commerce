0d957e421357807a701315637b7fc29da183fd2b
MoadJawabreh <moadjawbreh2001@yahoo.com>
2023-12-24 15:16:02 +0300

    H

---
+++ Medical-Tools/Medical-Tools/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Medical_Tools.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
