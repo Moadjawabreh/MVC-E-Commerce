2f80d38d8bf9542d22f599aa13b0fa401c732d60
Abedelhameed Alshorafa <abedalshorafa95@gmail.com>
2023-12-24 20:42:32 +0300

    model

---
+++ E-CommerceMedicalTools/E-CommerceMedicalTools/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_CommerceMedicalTools.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public double percentageOfDiscount { get; set; } = 1;
+
+        [ForeignKey("categoryId")]
+        public int? categoryId { get; set; }
+
+        public virtual Category category { get; set; }
+
+        public ICollection<User> users { get; set; }
+
+        public ICollection<Image> images { get; set; }
+
+    }
+}
