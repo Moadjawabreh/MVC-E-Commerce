eae4051bcd7c8852fd056540b727bd62cfbe61c5
Ayaodat <aya.odat@icloud.com>
2023-12-25 18:19:41 +0300

    edited home page

---
+++ E-CommerceMedicalTools/E-CommerceMedicalTools/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceMedicalTools.Models
+{
+    public enum Role
+    {
+        Admin,
+        User
+    }
+    public class User
+    {
+        public User()
+        {
+            this.products = new HashSet<Product>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string locationUrl { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+
+        public ICollection<Product> products { get; set; }
+    }
+}
